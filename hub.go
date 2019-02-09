b96c3dd9d49247fc89f70626cd8c1cc295fd230f
peristaltic <peristaltic@gmx.net>
2019-02-09 12:21:09 +0100

    update

--- hub.go
+++ hub.go
@@ -5,6 +5,7 @@ import "encoding/json"
 type HubMessage struct {
 	SenderLabel   string     `json:"sender_label"`
 	SenderID      int64      `json:"sender_id"`
+	SenderReason  string     `json:"sender_reason"`
 	ReceiverLabel string     `json:"receiver_label"`
 	ReceiverID    int64      `json:"receiver_id"`
 	Properties    Properties `json:"properties"`

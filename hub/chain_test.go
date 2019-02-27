a814829eacd7d821b939fd90bd39737a4263cb78
denkhaus <peristaltic@gmx.net>
2019-02-27 10:33:27 +0100

    update

---
+++ hub/chain_test.go
@@ -0,0 +1,69 @@
+package hub
+
+import (
+	"testing"
+
+	"github.com/lovoo/goka"
+	"github.com/stretchr/testify/assert"
+)
+
+var update = `
+{	
+	  "sender": "Photo",
+	  "sender_id": 2336,
+	  "receiver": "PersonConnector",
+	  "receiver_id": 4587,
+	  "operation": "updated", 
+	  "properties":{
+		  "name": "denkhaus",
+		  "visible": false
+	  }	
+}`
+
+func nodeInvisible(m Context) bool {
+	return m.Properties.MustBool("visible") == false
+}
+
+func TestChain(t *testing.T) {
+
+	codec := ContextCodec{}
+
+	m, err := codec.Decode([]byte(update))
+	assert.NoError(t, err, "decode raw message")
+
+	ctx, ok := m.(*Context)
+	assert.True(t, ok)
+
+	handlerTriggered := 0
+
+	var subordinates = Chain.From("Person").Or(
+		Chain.From("PersonPosition"),
+		Chain.From("Photo"),
+	)
+
+	subordinatesUpdated := subordinates.OnNodeUpdated()
+
+	condition := subordinatesUpdated.Do(
+		func(ctx goka.Context, m *Context) error {
+			handlerTriggered++
+			return nil
+		})
+
+	hit, err := condition.ApplyMessage(nil, ctx)
+	assert.NoError(t, err, "apply message")
+	assert.Equal(t, 1, handlerTriggered, "handler triggered")
+
+	assert.True(t, hit, "condition hit")
+
+	handlerTriggered = 0
+	var onSubordinatesInvisibled = subordinatesUpdated.With(nodeInvisible).
+		Do(func(ctx goka.Context, m *Context) error {
+			handlerTriggered++
+			return nil
+		})
+
+	hit, err = onSubordinatesInvisibled.ApplyMessage(nil, ctx)
+	assert.NoError(t, err, "apply message")
+	assert.Equal(t, 1, handlerTriggered, "handler triggered")
+	assert.True(t, hit, "condition hit")
+}
